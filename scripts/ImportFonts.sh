4f9905d74236261146d261e47a8b8f1b5c0926e4
AlphaRecon19 <chris@koolserve.uk>
2015-08-27 01:03:11 +0100

    Added Elegant Icons to the font import script and renamed it appropriately. Updated gitignore for the new font files.

--- scripts/ImportFonts.sh
+++ scripts/ImportFonts.sh
@@ -4,8 +4,18 @@
 git submodule update
 
 # Remove old fonts
+
+## FontAwesome
 rm -rf ../assets/fonts/FontAwesome*
 rm -rf ../assets/fonts/fontawesome-webfont*
 
+## elegant-icons
+rm -rf ../assets/fonts/elegant-icons*
+
+
 # Copy the new files
+## FontAwesome
 cp ../submodules/Font-Awesome/fonts/* ../assets/fonts/
+
+## elegant-icons
+cp ../submodules/elegant-icons/fonts/* ../assets/fonts/
